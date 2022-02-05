c97c5a0f0e948b83520e6813faa5707ef0ae9620
NinoKrb <nino.gralla@gmx.de>
2022-02-05 12:42:36 +0100

    Fix broken file path in background class

--- classes/background.py
+++ classes/background.py
@@ -9,7 +9,7 @@ from settings import Settings
 class Background(pygame.sprite.Sprite):
     def __init__(self, filename):
         super().__init__()
-        self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
+        self.image = pygame.image.load(os.path.join(Settings.path_background, filename)).convert_alpha()
         self.image = pygame.transform.scale(self.image, (Settings.window_width, Settings.window_height))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
