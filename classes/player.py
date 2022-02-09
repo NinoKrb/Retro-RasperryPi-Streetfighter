aca7faffe921b4eb521b144fa6e7e9a754ea034e
NinoKrb <nino.gralla@gmx.de>
2022-02-09 22:40:51 +0100

    Add simple movement & start animated actions

--- classes/player.py
+++ classes/player.py
@@ -1,3 +1,4 @@
+from operator import ne
 import os,sys,pygame
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -10,11 +11,17 @@ from classes.animation import Animation
 from classes.action import Action
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, id, size, fallback_image, animations):
+    def __init__(self, id, size, fallback_image, animations, speed, colorkey):
         super().__init__()
         self.id = id
         self.fallback_image = fallback_image
         self.size = size
+        self.colorkey = colorkey
+        self.speed = speed
+        self.direction = None
+        self.moving = False
+        self.pos = { 'x': 0, 'y': 0 }
+        self.flip = False
         self.animation_set = self.load_animation_set(animations)
         self.action_manager = Action({ 'name': 'idle', 'loop': True })
         self.animation_set.change_current_animation(self.action_manager.current_action['name'])
@@ -23,21 +30,58 @@ class Player(pygame.sprite.Sprite):
     def load_animation_set(self, animations):
         animation_objects = []
         for animation in animations:
-            animation_objects.append(Animation(animation, os.path.join('players', 'player_1', 'animations', animation)),)
+            animation_objects.append(Animation(animation, os.path.join('players', 'player_1', 'animations', animation), 100),)
 
         return AnimationSet('Player', animation_objects)
+
+    def move(self):
+        if self.moving:
+            if self.direction == 'right':
+                self.pos['x'] += self.speed
+            elif self.direction == 'left':
+                self.pos['x'] -= self.speed
+
+    def start_moving(self):
+        self.moving = True
+
+    def stop_moving(self):
+        self.moving = False    
+
+    def change_direction(self, direction):
+        self.direction = direction
+
+    def set_pos(self, x, y):
+        self.rect.top = y
+        self.rect.left = x
 
     def update_sprite(self, filename):
         self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
         self.image = pygame.transform.scale(self.image, self.size)
+        if self.flip:
+            self.image = pygame.transform.flip(self.image, self.flip, False)
+        self.image.set_colorkey(self.colorkey)
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
+        self.set_pos(self.pos['x'], self.pos['y'])
 
     def update(self):
         frame = self.animation_set.play_animation()
-        frame = os.path.join('players', 'player_1', 'animations', self.animation_set.current_animation['name'], frame)
         if frame:
+            frame = os.path.join('players', 'player_1', 'animations', self.animation_set.current_animation['name'], frame)
             self.update_sprite(frame)
+            if self.animation_set.current_animation['current_frame'] == 0:
+                next_action = self.action_manager.is_next_action_queued()
+                if next_action:
+                    print("Change to queued action")
+                    self.action_manager.change_action(next_action)
+                    self.animation_set.change_current_animation(next_action['name'])
+
+                elif self.action_manager.current_action['loop'] == False:
+                    print("Reset unloop action")
+                    self.action_manager.reset_action()
+                    self.animation_set.change_current_animation(self.action_manager.current_action['name'])
+                    
+        self.move()
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
--- main.py
+++ main.py
@@ -15,11 +15,13 @@ class Game():
         self.fps = pygame.time.Clock()
 
         self.background = Background(Settings.background_image)
-        self.player = Player(1, (250,250), 'fallback.png', ['idle', 'run'])
+        self.player = Player(1, (250,250), 'fallback.png', ['idle', 'run'], 2, (144, 200, 232))
         self.running = True
 
     def run(self):
         while self.running:
+            self.fps.tick(Settings.fps)
+            self.watch_for_events()
             self.update()
             self.draw()
 
@@ -30,6 +32,30 @@ class Game():
         self.background.draw(self.screen)
         self.player.draw(self.screen)
         pygame.display.flip()
+
+    def watch_for_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    self.player.change_direction('right')
+                    self.player.start_moving()
+                    self.player.flip = True
+                    self.player.action_manager.force_change_action('run', True)
+
+                if event.key == pygame.K_a:
+                    self.player.change_direction('left')
+                    self.player.start_moving()
+                    self.player.flip = False
+                    self.player.action_manager.force_change_action('run', True)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d or event.key == pygame.K_a:
+                    self.player.stop_moving()
+                    self.player.action_manager.reset_action()
+                    self.player.animation_set.change_current_animation(self.player.action_manager.current_action['name'])
 
 
 if __name__ == '__main__':
