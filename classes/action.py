d22d2a423ab30f645bd5cc72bd05975b404ba808
NinoKrb <nino.gralla@gmx.de>
2022-02-11 15:35:59 +0100

    Add clear queue method

--- classes/action.py
+++ classes/action.py
@@ -1,7 +1,7 @@
 class Action():
     def __init__(self, default_action=False):
         self.current_action = { 'name': None, 'loop': False }
-        self.next_action = { 'name': None, 'loop': False }
+        self.clear_queue()
         if default_action != False:
             self.default_action = default_action
             self.next_action = default_action
@@ -13,14 +13,17 @@ class Action():
     def queue_action(self, name, loop=False):
         self.next_action = { 'name': name, 'loop': loop }
 
+    def clear_queue(self):
+        self.next_action = None
+
     def force_change_action(self, name, loop=False):
         self.queue_action(name, loop)
         self.change_action(self.next_action)
+        self.clear_queue()
 
     def reset_action(self):
         if self.default_action != False:
             self.current_action = self.default_action
-            print("Reset Action")
 
     def is_next_action_queued(self):
         if self.current_action != self.next_action:
