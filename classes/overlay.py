0bf05b07677ffe47842f0506d332c7efa9ef36eb
NinoKrb <nino.gralla@gmx.de>
2022-03-02 21:09:21 +0100

    Add healthbar to overlay class

--- classes/overlay.py
+++ classes/overlay.py
@@ -10,9 +10,19 @@ class Overlay():
         self.elements.append(Avatar('player_1', self.player_1.avatar_image, Settings.avatar_size, (50,50)))
         self.elements.append(Avatar('player_2', self.player_2.avatar_image, Settings.avatar_size, (Settings.window_width - 50 - Settings.avatar_size[0],50), True))
 
+        self.healthbar_player_1 = Healthbar(self.player_1, self.player_1.health, self.player_1.max_health, (50 + Settings.avatar_size[0], 50))
+        self.healthbar_player_2 = Healthbar(self.player_2, self.player_2.health, self.player_2.max_health, (Settings.window_width - 50 - Settings.avatar_size[0], 50), 'right')
+
     def draw(self, screen):
         for element in self.elements:
             element.draw(screen)
+        
+        self.healthbar_player_1.draw(screen)
+        self.healthbar_player_2.draw(screen)
+
+    def update_healthbars(self, player_1, player_2):
+        self.healthbar_player_1.update_bar(player_1.health)
+        self.healthbar_player_2.update_bar(player_2.health)
 
 class OverlayElement(pygame.sprite.Sprite):
     def __init__(self, filename, size, pos, flip=False):
@@ -48,3 +58,37 @@ class Avatar(OverlayElement):
         print(joined_path)
         print(os.path.join(Settings.path_image, joined_path))
         super().update_sprite(joined_path)
+
+class Healthbar(pygame.sprite.Sprite):
+    def __init__(self, player, health, max, pos, direction='left'):
+        self.player = player
+        self.health = health
+        self.max = max
+        self.pos = pos
+        self.direction = direction
+        self.build_surface(self.health, self.max, self.pos, self.direction)
+
+    def build_surface(self, health, max, pos, direction):
+        health_surface = pygame.Surface((health, Settings.healthbar_height))
+        health_surface.fill(Settings.healthbar_health_color)
+        self.health_surface = { "surface": health_surface, "rect": health_surface.get_rect() }
+        self.set_pos(self.health_surface['rect'], *pos, direction)
+        
+        max_surface = pygame.Surface((max, Settings.healthbar_height))
+        max_surface.fill(Settings.healthbar_blank_color)
+        self.max_surface = { "surface": max_surface, "rect": max_surface.get_rect() }
+        self.set_pos(self.max_surface['rect'], *pos, direction)
+
+    def update_bar(self, health):
+        self.health_surface['rect'].width = health
+
+    def draw(self, screen):
+        screen.blit(self.max_surface['surface'], self.max_surface['rect'])
+        screen.blit(self.health_surface['surface'], self.health_surface['rect'])
+
+    def set_pos(self, rect, x, y, direction='left'):
+        rect.top = y
+        if direction == 'left':
+            rect.left = x
+        else:
+            rect.right = x
