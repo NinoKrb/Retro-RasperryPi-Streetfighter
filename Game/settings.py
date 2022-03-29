ded94592f90f75d82c3a2d9f063dcb20a20f5627
Nino Gralla <nino.gralla@gmx.de>
2022-03-30 01:17:09 +0200

    Add use_raspberry_pi configuration

--- Game/settings.py
+++ Game/settings.py
@@ -23,6 +23,8 @@ class Settings():
     # Assets
     background_image = "background.png"
 
+    use_raspberry_pi = False
+
     # Player
     player_size_1 = (250,250)
     player_size_2 = (200,200)
@@ -36,11 +38,12 @@ class Settings():
     player_y_momentum = 0.5
     player_max_y_momentum = 10
     player_max_jumps = 2
-    player_health = 5000
+    player_health = 1000
+    player_collide_ratio = 0.55
 
     # Overlay
     avatar_size = (50,50)
     healthbar_height = 25
     healthbar_health_color = (201, 54, 54)
     healthbar_blank_color = (199, 199, 199)
-    healthbar_width_factor = 20
+    healthbar_width_factor = 3
